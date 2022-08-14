27b96b1d0354c9aaf9ffd3d48ea2b914db52679e
[SW2/서울/2반]김한얼 <gksdjf80@naver.com>
2022-08-14 21:03:42 +0900

    [create] Search

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,11 +1,29 @@
 import Link from 'next/link';
+import { ReactNode } from 'react';
+
+type ButtonProps = {
+  link: string;
+  children: ReactNode;
+  onClick?: (event: MouseEvent) => void;
+};
 
 export default function Button(props: any) {
+  if (props.link) {
+    return (
+      <Link href={props.link}>
+        <a className=' block no-underline cursor-pointer border-solid border border-emerald-300 bg-emerald-500 rounded-md text-emerald-200 py-2 px-6 hover:bg-teal-500 hover:border-teal-500 active:bg-teal-500 active:border-teal-500'>
+          {props.children}
+        </a>
+      </Link>
+    );
+  }
+
   return (
-    <Link href={props.link}>
-      <a className=' block no-underline cursor-pointer border-solid border border-emerald-300 bg-emerald-500 rounded-md text-emerald-200 py-2 px-6 hover:bg-teal-500 hover:border-teal-500 active:bg-teal-500 active:border-teal-500'>
-        {props.children}
-      </a>
-    </Link>
+    <button
+      onClick={props.onClick}
+      className=' block no-underline cursor-pointer border-solid border border-emerald-300 bg-emerald-500 rounded-md text-emerald-200 py-2 px-6 hover:bg-teal-500 hover:border-teal-500 active:bg-teal-500 active:border-teal-500'
+    >
+      {props.children}
+    </button>
   );
 }
